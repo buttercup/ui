3dee6e1ef41b3d96078a1d04c2f10084ef1fa4f9
Sallar Kaboli <sallar.kaboli@gmail.com>
2017-06-10 15:15:14 +0300

    Upgrade dependencies

--- src/components/generator.js
+++ src/components/generator.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { generate, generateWords } from 'buttercup-generator';
 import { colors } from '../variables';
