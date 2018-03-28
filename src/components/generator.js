0ede7c6695aea919cc91a1ba1f4faaf3d988db73
Perry Mitchell <perry@perrymitchell.net>
2018-03-28 22:26:23 +0300

    Refactor styled components for generator

--- src/components/generator.js
+++ src/components/generator.js
@@ -16,6 +16,7 @@ const StyledPopover = styled(Popover)`
   }
 
   .Popover-body {
+    width: 300px;
     display: inline-flex;
     padding: 0;
     flex-direction: column;
@@ -26,7 +27,82 @@ const Password = styled(ColoredDigits)`
     color: ${colors.BRAND_PRIMARY};
   }
 `;
+const Body = styled.div`
+  background: ${colors.DARK_SECONDARY};
+  padding: 12px;
+  color: #fff;
+  border-radius: 5px;
+  font-weight: 300;
+`;
+const PasswordContainer = styled.pre`
+  font-size: 0.9rem;
+  padding: 6px;
+  margin: 0;
+  background-color: ${colors.BLACK_25};
+  border-radius: 3px;
+  overflow: hidden;
+  font-weight: 400;
+  cursor: copy;
+`;
+const GenerationTypes = styled.div`
+  margin: 12px 0;
+  font-size: 0.9rem;
 
+  label {
+    display: block;
+  }
+
+  small {
+    color: ${colors.GRAY_DARK};
+  }
+`;
+const GeneratorLabel = styled.div`
+  display: block;
+`;
+const GeneratorRangeLabel = styled.div`
+  display: flex;
+
+  span {
+    flex: 0;
+    background-color: ${colors.BLACK_25};
+    padding: 0 3px;
+    border-radius: 3px;
+    font-family: monospace;
+    width: 30px;
+    margin-left: 6px;
+  }
+
+  input {
+    flex: 1;
+  }
+`;
+const GeneratorFieldset = styled.fieldset`
+  border-color: ${colors.WHITE_50};
+  border-radius: 3px;
+`;
+const GeneratorLegend = styled.legend`
+  text-transform: uppercase;
+  padding: 0 6px;
+  font-size: 0.8em;
+`;
+const GeneratorControls = styled.div`
+  display: flex;
+  margin-top: 12px;
+  align-items: flex-start;
+  flex-direction: row;
+
+  button {
+    width: 50%;
+
+    &:first-child {
+      margin-right: 6px;
+    }
+  }
+`;
+
+/**
+ * Generator component
+ */
 export class GeneratorUserInterface extends Component {
   static propTypes = {
     onGenerate: PropTypes.func.isRequired
@@ -42,11 +118,7 @@ export class GeneratorUserInterface extends Component {
   };
 
   characterSetEnabled(setName) {
-    return (
-      this.state.config.randomCharacters.enabledCharacterSets.indexOf(
-        setName
-      ) >= 0
-    );
+    return this.state.config.randomCharacters.enabledCharacterSets.indexOf(setName) >= 0;
   }
 
   componentDidMount() {
@@ -72,9 +144,7 @@ export class GeneratorUserInterface extends Component {
   }
 
   toggleCharacterSet(setName) {
-    const currentCharacterSets = [
-      ...this.state.config.randomCharacters.enabledCharacterSets
-    ];
+    const currentCharacterSets = [...this.state.config.randomCharacters.enabledCharacterSets];
     const charsetIndex = currentCharacterSets.indexOf(setName);
     if (charsetIndex >= 0) {
       currentCharacterSets.splice(charsetIndex, 1);
@@ -137,36 +207,32 @@ export class GeneratorUserInterface extends Component {
 
   render() {
     return (
-      <div className={this.props.className}>
-        <pre
-          className="password"
-          role="content"
-          onClick={e => selectElementContents(e.target)}
-        >
+      <Body>
+        <PasswordContainer role="content" onClick={e => selectElementContents(e.target)}>
           <Password value={this.state.password} />
-        </pre>
-        <div className="types">
-          <label>
+        </PasswordContainer>
+        <GenerationTypes>
+          <GeneratorLabel>
             <input
               type="radio"
               checked={this.state.config.mode === 'characters'}
               onChange={() => this.changeType('characters')}
             />{' '}
             Characters
-          </label>
-          <label>
+          </GeneratorLabel>
+          <GeneratorLabel>
             <input
               type="radio"
               checked={this.state.config.mode === 'words'}
               onChange={() => this.changeType('words')}
             />{' '}
             Words
-          </label>
-        </div>
+          </GeneratorLabel>
+        </GenerationTypes>
         <If condition={this.state.config.mode === 'characters'}>
-          <fieldset className="set">
-            <legend>Options</legend>
-            <label className="rangeLabel">
+          <GeneratorFieldset>
+            <GeneratorLegend>Options</GeneratorLegend>
+            <GeneratorRangeLabel>
               <input
                 type="range"
                 value={this.state.config.randomCharacters.length}
@@ -175,71 +241,76 @@ export class GeneratorUserInterface extends Component {
                 onChange={e => this.changeLength(e)}
               />
               <span>{this.state.length}</span>
-            </label>
-            <label>
+            </GeneratorRangeLabel>
+            <GeneratorLabel>
               <input
                 type="checkbox"
                 checked={this.characterSetEnabled('UPPERCASE')}
                 onChange={() => this.toggleCharacterSet('UPPERCASE')}
               />{' '}
               Uppercase Letters
-            </label>
-            <label>
+            </GeneratorLabel>
+            <GeneratorLabel>
               <input
                 type="checkbox"
                 checked={this.characterSetEnabled('LOWERCASE')}
                 onChange={() => this.toggleCharacterSet('LOWERCASE')}
               />{' '}
               Lowercase Letters
-            </label>
-            <label>
+            </GeneratorLabel>
+            <GeneratorLabel>
               <input
                 type="checkbox"
                 checked={this.characterSetEnabled('DIGITS')}
                 onChange={() => this.toggleCharacterSet('DIGITS')}
               />{' '}
               Digits
-            </label>
-            <label>
+            </GeneratorLabel>
+            <GeneratorLabel>
               <input
                 type="checkbox"
                 checked={this.characterSetEnabled('SPACE')}
                 onChange={() => this.toggleCharacterSet('SPACE')}
               />{' '}
               Space
-            </label>
-            <label>
+            </GeneratorLabel>
+            <GeneratorLabel>
               <input
                 type="checkbox"
                 checked={this.characterSetEnabled('UNDERSCORE_DASH')}
                 onChange={() => this.toggleCharacterSet('UNDERSCORE_DASH')}
               />{' '}
               Underscore &amp; Dash
-            </label>
-            <label>
+            </GeneratorLabel>
+            <GeneratorLabel>
               <input
                 type="checkbox"
                 checked={this.characterSetEnabled('SYMBOLS')}
                 onChange={() => this.toggleCharacterSet('SYMBOLS')}
               />{' '}
               Symbols
-            </label>
-          </fieldset>
+            </GeneratorLabel>
+          </GeneratorFieldset>
         </If>
-        <div className="buttons">
+        <GeneratorControls>
           <Button onClick={() => this.generatePassword()} primary>
             Generate
           </Button>
           <Button onClick={() => this.onGenerate()} dark>
             Use This
           </Button>
-        </div>
-      </div>
+        </GeneratorControls>
+      </Body>
     );
   }
 }
 
-class GeneratorWithPopover extends GeneratorUserInterface {
+/**
+ * Generator component in a popover
+ * @augments GeneratorUserInterface
+ * @see GeneratorUserInterface
+ */
+export class Generator extends GeneratorUserInterface {
   render() {
     const { children, isOpen, className, ...rest } = this.props;
     return (
@@ -249,84 +320,3 @@ class GeneratorWithPopover extends GeneratorUserInterface {
     );
   }
 }
-
-export const Generator = styled(GeneratorWithPopover)`
-  width: 300px;
-  background: ${colors.DARK_SECONDARY};
-  padding: 12px;
-  color: #fff;
-  border-radius: 5px;
-  font-weight: 300;
-
-  label {
-    display: block;
-  }
-
-  .set {
-    border-color: ${colors.WHITE_50};
-    border-radius: 3px;
-
-    legend {
-      text-transform: uppercase;
-      padding: 0 6px;
-      font-size: 0.8em;
-    }
-  }
-
-  .password {
-    font-size: 0.9rem;
-    padding: 6px;
-    margin: 0;
-    background-color: ${colors.BLACK_25};
-    border-radius: 3px;
-    overflow: hidden;
-    font-weight: 400;
-    cursor: copy;
-  }
-
-  .rangeLabel {
-    display: flex !important;
-
-    span {
-      flex: 0;
-      background-color: ${colors.BLACK_25};
-      padding: 0 3px;
-      border-radius: 3px;
-      font-family: monospace;
-      width: 30px;
-      margin-left: 6px;
-    }
-
-    input {
-      flex: 1;
-    }
-  }
-
-  .buttons {
-    display: flex;
-    margin-top: 12px;
-    align-items: flex-start;
-    flex-direction: row;
-
-    button {
-      width: 50%;
-
-      &:first-child {
-        margin-right: 6px;
-      }
-    }
-  }
-
-  .types {
-    margin: 12px 0;
-    font-size: 0.9rem;
-
-    label {
-      display: block;
-    }
-
-    small {
-      color: ${colors.GRAY_DARK};
-    }
-  }
-`;
