c95ebbc26a7e5c446ad4628157ddf9f6b3648180
Perry Mitchell <perry@perrymitchell.net>
2019-03-15 18:47:45 +0200

    Output icons to dist

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,18 @@ module.exports = {
           },
           'css-loader' // translates CSS into CommonJS
         ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: "[name].[ext]",
+              outputPath: "icons"
+            }
+          }
+        ]
       }
     ]
   },
