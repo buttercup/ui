0467543181fb02af570aacd4b77578f1cf136260
Perry Mitchell <perry@perrymitchell.net>
2019-07-14 23:46:14 +0300

    Remove iterated test entries

--- stories/vault.js
+++ stories/vault.js
@@ -33,16 +33,6 @@ function createArchive() {
     .createEntry('Gate lock combination')
     .setProperty('username', 'test')
     .setProperty('password', '4812');
-  // for (let i = 0; i < 20; i++) {
-  //   general
-  //     .createEntry('Gate lock combination ' + i)
-  //     .setProperty('username', 'test')
-  //     .setProperty('password', '4812')
-  //     .setProperty(
-  //       'url',
-  //       'https://www.amazon.com/ap/signin?openid.assoc_handle=aws&openid.return_to=https%3A%2F%2Fsignin.aws.amazon.com%2Foauth%3Fresponse_type%3Dcode%26client_id%3Darn%253Aaws%253Aiam%253A%253A015428540659%253Auser%252Fhomepage%26redirect_uri%3Dhttps%253A%252F%252Feu-west-1.console.aws.amazon.com%252Fconsole%252Fhome%253Fregion%253Deu-west-1%2526state%253DhashArgs%252523%2526isauthcode%253Dtrue%26noAuthCookie%3Dtrue&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&action=&disableCorpSignUp=&clientContext=&marketPlaceId=&poolName=&authCookies=&pageId=aws.ssop&siteState=registered%2Cen_US&accountStatusPolicy=P1&sso=&openid.pape.preferred_auth_policies=MultifactorPhysical&openid.pape.max_auth_age=120&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&server=%2Fap%2Fsignin%3Fie%3DUTF8&accountPoolAlias=&forceMobileApp=0&language=en_US&forceMobileLayout=0'
-  //     );
-  // }
   const notes = archive.createGroup('Notes');
   notes
     .createEntry('Meeting notes 2019-02-01')
