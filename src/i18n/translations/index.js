d3d419922b63b2f4c76c195a9b279151628370a3
Perry Mitchell <perry@perrymitchell.net>
2021-03-21 15:51:05 +0200

    Add Finnish and Swedish translations

--- src/i18n/translations/index.js
+++ src/i18n/translations/index.js
@@ -1,5 +1,6 @@
 import de from './de.json';
 import en from './en.json';
+import fi from './fi.json';
 import fr from './se.json';
 import ja from './ja.json';
 import pl from './pl.json';
@@ -9,6 +10,7 @@ import se from './se.json';
 export default {
   de,
   en,
+  fi,
   fr,
   ja,
   pl,
