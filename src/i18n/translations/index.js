a11abf60a0d30d852ff0b8fac8e7e195b84fa926
Perry Mitchell <perry@perrymitchell.net>
2021-05-09 18:46:37 +0300

    Re-order translations

--- src/i18n/translations/index.js
+++ src/i18n/translations/index.js
@@ -1,23 +1,23 @@
 import de from './de.json';
 import en from './en.json';
+import es from './es.json';
 import fi from './fi.json';
 import fr from './se.json';
+import gl from './gl.json';
 import ja from './ja.json';
 import pl from './pl.json';
 import ru from './ru.json';
 import se from './se.json';
-import gl from './gl.json';
-import es from './es.json';
 
 export default {
   de,
   en,
+  es,
   fi,
   fr,
+  gl,
   ja,
   pl,
   ru,
-  se,
-  gl,
-  es
+  se
 };
