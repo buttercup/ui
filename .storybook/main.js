e05dc0a0bfc44c94c6b13ef86299e798598f6d43
Perry Mitchell <perry@perrymitchell.net>
2022-02-19 22:47:54 +0200

    Fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,39 @@ module.exports = {
     core: {
         builder: "webpack5"
     },
-    stories: ["../stories/index.js"]
+    stories: ["../stories/index.js"],
+    webpackFinal: async (config, {
+        configType
+    }) => {
+        return {
+            ...config,
+            module: {
+                ...config.module,
+                rules: [
+                    ...config.module.rules,
+                    {
+                        test: /\.m?js$/,
+                        exclude: /(node_modules)/,
+                        use: {
+                            loader: 'babel-loader'
+                        }
+                    },
+                    {
+                        test: /\.dat$/,
+                        use: [
+                            'arraybuffer-loader'
+                        ]
+                    }
+                ]
+            },
+            resolve: {
+                ...config.resolve,
+                fallback: {
+                    crypto: false,
+                    fs: false,
+                    ...config.resolve.fallback
+                }
+            }
+        };
+    }
 };
